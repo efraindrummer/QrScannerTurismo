248c97440421118a05c22403496df563c604fa8f
Efrain May <efrain_may_75@hotmail.com>
2021-09-03 15:17:05 -0500

    open the camera listo

--- lib/src/screens/scanner_page.dart
+++ lib/src/screens/scanner_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScannerPage extends StatelessWidget {
   const ScannerPage({Key key}) : super(key: key);
@@ -6,10 +7,30 @@ class ScannerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Scanner"),
+      ),
       body: Container(
         child: Center(
-          child: Text("Hello"),
+          child: Column(
+            children: [
+              SizedBox(height: 200),
+              Image.asset("assets/image/splash_image.png", width: 200),
+              TextButton(
+                onPressed: () async {
+                  String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('0xFF396afc', 'Cancelar', false, ScanMode.QR);
+                  print(barcodeScanRes);
+                }, 
+                child: Text(
+                  "Scanear Ahora!", 
+                  style: TextStyle(
+                    color: Colors.black, 
+                    fontSize: 24,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
